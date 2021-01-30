2cfb1ad61983f09694fda9beafcce49a9219725a
Christophxx <59886549+Christophxx@users.noreply.github.com>
2021-01-30 17:27:56 +0100

    ADD : Add view File for Error Page

---
+++ views/404.php
@@ -0,0 +1,34 @@
+<?php 
+
+//SET THE PATH TO THE INCLUDES
+set_include_path ('C:/xampp/htdocs/includes');
+include 'sidebar.php';
+include 'config.php';
+
+?>
+
+<style>
+
+.head {
+    top: 200px;
+    font-size: 60px;
+    font-weight: bold;
+}
+
+.text {
+    top: 180px;
+}
+</style>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-center head">
+        <p>404</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center text">
+        <p>.. Oops! Something is Wrong!</p>
+        </div>
+    </div>
+</div>
